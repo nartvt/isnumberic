04e750b02fb64a02be38f9ad031fe821a59a2f58
iatnavnart <tooyoutooyou@gmail.com>
2021-05-26 18:49:57 +0700

    refactor code

--- app.js
+++
@@ -1,9 +0,0 @@
-var mailformat = /^[a-zA-Z][0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
-var email = "_a9999@gmail.com"
-if (email.match(mailformat)) {
-    console.log("Email valid")
-} else {
-    console.log("Email invalid")
-}
-
-// https://html.spec.whatwg.org/multipage/input.html#valid-e-mail-address
